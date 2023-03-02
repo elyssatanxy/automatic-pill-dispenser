a7fa91968ca28c5bdc4cf63bf4c33877f0a37963
kenthough <111831749+kenthough@users.noreply.github.com>
2023-03-02 16:53:00 +0800

    Update haiyo.py
    completed the todos, experimenting layout options and able to scale it fullscreen

--- haiyo.py
+++ haiyo.py
@@ -1,4 +1,4 @@
-from guizero import App, Text, PushButton
+from guizero import App, Box, Text, PushButton, Window
 from guizero import info
 import serial
 from serial.serialutil import SerialException
@@ -7,56 +7,62 @@ from serial.tools import list_ports
 # NOTE: connecting and disconnecting functions will be removed in final implementation, just for me to check
 # connect to microbit via serial port
 def connect():
-    try:
-        ser.open()
-    except SerialException:
-        info("Error", "Unplug microbit and try again")
- 
- 
+	try:
+		ser.open()
+	except SerialException:
+		info("Error", "Unplug microbit and try again")
+		
+
 # disconnect from microbit
 def disconnect():
-    ser.close()
-    
+	ser.close()
 
-# send number for timer 
+
+# send number for timer
 def send_data():
-    global timer
-    send_this = str(timer) + "\n"
-    print(send_this)
-    
-    try:
-        ser.write(send_this.encode())
-    except SerialException:
-        info("Error", "Could not send.")
-        
-        
+	global timer
+	send_this = str(timer) + "\n"
+	print("sending value: ", send_this)
+	
+	try:
+		ser.write(send_this.encode())
+	except SerialException:
+		info("Error", "Could not send.")
+
 # increment timer value
 def increment_timer():
-    global timer
-    timer += 1
-    print("updated timer to", timer)
+	global timer
+	timer += 1
+	print("updated timer to", timer)
+	timer_text.value = timer
 
 
 # decrement timer value
 def decrement_timer():
-    global timer
-    timer -= 1
-    print("updated timer to", timer)
+	global timer
+	if (timer-1 > -1):
+		timer -= 1
+		print("updated timer to", timer)
+		timer_text.value = timer
+	else: 
+		print("timer cannot be negative", timer)
 
-
-# TODO: stop and reset the timer immediately
+# COMPLETED: stop and reset the timer immediately
 def reset_timer():
-    pass
-
+	global timer
+	timer = 0
+	send_data()
+	timer_text.value = timer
+	print("timer reset to 0")
 
 # find port number of microbit
 def find_microbit_comport():
-    ports = list(list_ports.comports())
-    for p in ports:
-        if (p.pid == 516) and (p.vid == 3368):
-            return str(p.device)
-        
-    
+	ports = list(list_ports.comports())
+	for p in ports:
+		if (p.pid == 516) and (p.vid == 3368):
+			return str(p.device)
+	
+
 # SET UP CONNECTION
 ser = serial.Serial()
 ser.baudrate = 115200
@@ -65,22 +71,24 @@ ser.open()
 
 
 # START OF GUI
-timer = 3
-
 app = App(title="pill dispenser")
 
-intro = Text(app, text = "test")
+menu = Box(app, width="fill", align="top")
+intro = Text(menu, text="Pill Dispensing", size=40, align="top")
 
 # NOTE: these buttons will be removed to avoid any tampering
-connect_button = PushButton(app, text = "Connect", command=connect)
-disconnect_button = PushButton(app, text = "Disconnect", command=disconnect)
+connect_button = PushButton(menu, text = "Connect", command=connect, align="right")
+disconnect_button = PushButton(menu, text = "Disconnect", command=disconnect, align="right")
 
 # BUTTONS FOR ADJUSTING TIMER
 increment_button = PushButton(app, text = "+", command=increment_timer)
 decrement_button = PushButton(app, text = "-", command=decrement_timer)
+reset_button = PushButton(app, text = "reset", command=reset_timer)
 send_button = PushButton(app, text="Send", command=send_data)
 
-# TODO: gui does not reflect the changes in timer values
-timer_text = Text(app, text = timer)
+# COMPLETED: gui does not reflect the changes in timer values
+timer = 0
+timer_text = Text(app, text = timer, align="top")
 
+# app.set_full_screen()
 app.display()
